8a074845aa62b84fb8fcf7070cde08242de7f081
sireliah <pgolabb@gmail.com>
2017-06-17 14:23:46 +0200

    Order of exports.

--- deployment/run_PANkreator.sh
+++ deployment/run_PANkreator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=$HOME/PANkreator/lib/lib/
 source $HOME/PANkreator/panvenv/bin/activate
+export LD_LIBRARY_PATH=$HOME/PANkreator/lib/lib/
 cd $HOME/PANkreator/pbc/
 python3 pga.py
